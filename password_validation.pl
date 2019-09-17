76d6f6d8c29c47b653f5295c2f95f5bc2eb1e403
syunwei <syunweicheng@gmail.com>
2019-09-17 09:39:18 +0800

    Use lookahead to validate password.

--- password_validation.pl
+++ password_validation.pl
@@ -4,28 +4,20 @@ use strict;
 use warnings;
 
 sub passwordValidate {
-    my $userPassword = shift;             #Get input arguments: password 
-    my $len = length($userPassword);      #Password length
-    my $PASS = 1;                         #return 1 when pass
-    # print $len;
-
-    #regular expression patterns
-    my $mix_cases_pat = qr/([A-Z].*[a-z])|([a-z].*[A-Z])/x; # Mixed cases pattern
-    my $numbers_pat = qr/[0-9]/x;                           # Numbers pattern  
-    my $symboss_pat = qr/[^\d\w]|\_/x;                      # Symbols pattern
-
-    return $PASS if $len >= 20;                                     # >= 20: Any character desired.
-    return !$PASS if $len < 8;                                     # <  8: At least 8 Length.
-    return !$PASS if !($userPassword =~ $mix_cases_pat);            #Failed: Not containes mied cased characters.
-    return !$PASS if !($userPassword =~ $numbers_pat) && $len < 16; #Failed: Not containes numbers if len < 16.
-    return !$PASS if !($userPassword =~ $symboss_pat) && $len < 12; #Failed: Not containes symbs if len < 12.
-    return $PASS
+    my $userPassword = shift; 
+    my $validation = qr/
+        \A(?=.{20,}\z)|
+        \A(?=.{16,}\z)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])|
+        \A(?=.{12,}\z)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=[\D]*[\d])|
+        \A(?=.{8,}\z)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=[\D]*[\d])(?=[\w\d]*[\W\D\_])
+    /x;
+    return $userPassword =~ m/$validation/g;
 }
 eval {
-    my @failed_password = ('8liX=F', '#/@{[-<<', '*-2)%.|&', '2]0g"\*t', 'W)!559(*', 'C%J)F."?}T+S', '%/zbe}v,mljv', 'bdoqlixsrqxpqpmu');
-    my @passed_password = ('MTt{2s.X', 'zBqR88KUnDf6', 'asdRincvBLSzWYgt', 'WmBe87;G6Y{Pr-8HFpQ');
-
+    # my @failed_password = ('8liX=F', '#/@{[-<<', '*-2)%.|&', '2]0g"\*t', 'W)!559(*', 'C%J)F."?}T+S', '%/zbe}v,mljv', 'bdoqlixsrqxpqpmu');
+    my @passed_password = ('MTt{2s.X', 'zBqR88KUnDf6', 'asdRincvBLSzWYgt', 'WmBe87;G6Y{Pr-8HFpQ', '...@...._...........','bdoqsslixsrqxpqpmuss');
     foreach my $password (@passed_password) {
+        print length($password);
         print passwordValidate($password) ? "Passed\n" : "Failed\n";
     }
 };
