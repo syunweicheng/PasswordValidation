00d849e9c71f65b7ac0a4840a877e054ebf8ec2d
syunwei <syunweicheng@gmail.com>
2019-09-18 22:45:49 +0800

    fix bug: \w = [a-z A-Z 0-9]

--- password_validation.pl
+++ password_validation.pl
@@ -9,15 +9,15 @@ sub passwordValidate {
         \A(?=.{20,}\z)|
         \A(?=.{16,}\z)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])|
         \A(?=.{12,}\z)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=[\D]*[\d])|
-        \A(?=.{8,}\z)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=[\D]*[\d])(?=[\w\d]*[\W\D\_])
+        \A(?=.{8,}\z)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=[\D]*[\d])(?=[\w]*[\W\_])
     /x;
     return $userPassword =~ m/$validation/g;
 }
 eval {
-    # my @failed_password = ('8liX=F', '#/@{[-<<', '*-2)%.|&', '2]0g"\*t', 'W)!559(*', 'C%J)F."?}T+S', '%/zbe}v,mljv', 'bdoqlixsrqxpqpmu');
+    # my @failed_password = ('abcEF123456','8liX=F', '#/@{[-<<', '*-2)%.|&', '2]0g"\*t', 'W)!559(*', 'C%J)F."?}T+S', '%/zbe}v,mljv', 'bdoqlixsrqxpqpmu');
     my @passed_password = ('MTt{2s.X', 'zBqR88KUnDf6', 'asdRincvBLSzWYgt', 'WmBe87;G6Y{Pr-8HFpQ', '...@...._...........','bdoqsslixsrqxpqpmuss');
     foreach my $password (@passed_password) {
-        print length($password);
+        # print length($password);
         print passwordValidate($password) ? "Passed\n" : "Failed\n";
     }
 };
